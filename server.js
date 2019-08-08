709d7bcf8d9ede89226bc55ccdbbee45833c60f6
Jonathon Langkammer <44826098+ConditionalStatementLifestyle@users.noreply.github.com>
2019-08-08 10:58:47 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 // GraphQL schema
 var schema = buildSchema(`
-
     input OrderInput {
         description: String!
         total: Float!
@@ -13,12 +12,19 @@ var schema = buildSchema(`
         amount: Float!
         note: String
     },
+    input OrderPayInput {
+        description: String!
+        total: Float!
+        paymentAmount: Float!
+        note: String
+    },
     type Query {
         orders: [Order]
     },
     type Mutation {
         createOrder(input: OrderInput): Order
         applyPayment(input: PaymentInput): Payment
+        placeOrderAndPay(input: OrderPayInput): Order
     },
     type Order {
         id: ID!
@@ -33,9 +39,9 @@ var schema = buildSchema(`
         appliedAt: String!
         note: String
     }
-
 `);
 
+// ********************* Order & Payment Classes *********************
 class Order {
     constructor(id, {description, total}) {
         this.id = id;
@@ -65,38 +71,68 @@ class Payment {
         let date = dateTime[0].slice(0,-1)
         let time = dateTime[1].slice(0,-3)
         let AMPM = dateTime[2]
-        return `${time} ${AMPM} ${date}`
+        return `${time}${AMPM} ${date}`
     }
 }
 
+// ********************* array to hold all orders *********************
 var ordersData = []
+// ********************* object to hold all order ids *********************
+// This gives us O(1) time complexity when checking to see if order exists.
+// Optimally ordersData would be an object as well but I had trouble implementing this.
+// Might be able to get around array requirement by nesting a single object inside of an array
+// although that solution seems messy.
 var orderIdLookup = {};
+
+// ********************* helper function to create order *********************
+handleCreateOrder = (input) => {
+    let id = require('crypto').randomBytes(10).toString('hex');
+    while (orderIdLookup[id]) {
+        id = require('crypto').randomBytes(10).toString('hex');
+    }
+    orderIdLookup[id] = true;
+    let newOrder = new Order(id, input)
+    ordersData.push(newOrder)
+    return newOrder
+}
+
+// ********************* helper function to handle order payment *********************
+handleApplyPayment = (input) => {
+    const {orderId, amount} = input
+    if (!orderIdLookup[orderId]) {
+        throw new Error(`no order exists with id ${orderId}`)
+    }
+    ordersData.filter(order => {
+        if (order.id === orderId) {
+            if (order.balanceDue === 0) {
+                throw new Error("There is no balance due on this order.")
+            }
+            else if (order.balanceDue < amount) {
+                throw new Error(`The applied payment of $${amount} exceeds the balance due ($${order.balanceDue}). Please resubmit the payment.`)
+            }
+            else {
+                order.applyPayment(input)
+                return order
+            }
+        }
+    })
+}
 
 var root = {
     orders: () => {
         return ordersData;
     },
-    createOrder: ({input}) => {        
-        let id = require('crypto').randomBytes(10).toString('hex');
-        while (orderIdLookup[id]) {
-            id = require('crypto').randomBytes(10).toString('hex');
-        }
-        orderIdLookup[id] = true;
-        let newOrder = new Order(id, input)
-        ordersData.push(newOrder)
-        return newOrder
+    createOrder: ({input}) => {      
+        return handleCreateOrder(input)
     },
     applyPayment: ({input}) => {
-        const {orderId} = input
-        if (!orderIdLookup[orderId]) {
-            throw new Error(`no order exists with id ${orderId}`)
-        }
-        ordersData.filter(order => {
-            if (order.id === orderId) {
-                order.applyPayment(input)
-                return order
-            }
-        })
+        return handleApplyPayment(input)
+    },
+    placeOrderAndPay: ({input}) => {
+        let {description, total, paymentAmount, note} = input
+        let order = handleCreateOrder({description: description, total: total})
+        handleApplyPayment({orderId: order.id, amount: paymentAmount, note: note})
+        return order
     }
 };
 
@@ -108,3 +144,4 @@ app.use('/graphql', graphqlHTTP({
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
+
